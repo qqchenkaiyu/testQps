0c3e8bdf25dad26d874aafaa70bc30df76a6ba5f
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2022-01-17 20:15:57 +0800

    Create QPSContext.java

---
+++ src/main/java/com/example/demo/QPSContext.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ */
+
+package com.example.demo.util;
+
+import lombok.Data;
+
+@Data
+public class QPSContext {
+
+    int expectQps;
+    String url;
+}
